70c3382013a974e53b4ff0f439a04d85ad2ee7ac
Vitor Morato <vitor.morato80@gmail.com>
2023-05-10 20:21:57 -0300

    Fourth commit

---
+++ HTML5 + CSS3 - Criação de site de catálogo de professores/myteacher/static/js/script.js
@@ -0,0 +1,15 @@
+let teacherForms = document.getElementsByClassName("teacher-contract")
+for (let teacherForm of teacherForms) {
+    teacherForm.addEventListener("submit", function (event){
+        event.preventDefault()
+        console.log('Executou o submit')
+
+    let price = teacherForm.price.value
+    let hours = teacherForm.hours.value
+    let total = price * hours
+
+
+
+    alert('O valor total do contrato é de R$ ' + total)
+    })
+}
